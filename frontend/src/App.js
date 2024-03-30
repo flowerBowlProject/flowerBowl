ba4452e5d0e43628fe88b271e544ec0e61802765
Joy <wlsgml235@gmail.com>
2024-03-30 20:46:32 +0900

    Chore: MUI 기초세팅

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
-import Header from './MainPage/Header';
-import Footer from './MainPage/Footer';
-
+// import Header from './MainPage/Header';
+// import Footer from './MainPage/Footer';
+import Inputbutton from './Profile/Inputbutton';
 
 const App =()=> {
   return (
     <div>
-      <Header/>
-      <Footer/>
+      {/* <Header/>
+      <Footer/> */}
+      <Inputbutton/>
     </div>
   );
 }
---
+++ frontend/src/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+	palette: {
+      main: {
+        yl: '#FEFBEC',
+        gr: '#B0A695',
+        or: '#F6C47B',
+        br: '#B9835C'
+      },
+    },
+    typography:{
+        fontFamily: 'GowunBatang-Bold'
+    }
+});
