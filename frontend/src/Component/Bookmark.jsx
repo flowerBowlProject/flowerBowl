d71c0a04315450c75983db224b93bd42b83e5163
김희진 <95160590+Khjin06k@users.noreply.github.com>
2024-03-30 21:34:14 +0900

    Feat: 공통 컴포넌트 및 viewList 틀

---
+++ frontend/src/Component/Bookmark.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import TurnedInNotIcon from '@mui/icons-material/TurnedInNot';
+import TurnedInIcon from '@mui/icons-material/TurnedIn';
+
+const Bookmark = () => {
+    return (
+        <div style={{position: 'absolute'}}>
+            {/* 즐겨찾기 여부에 따른 채움 / 빈 아이콘 - api 연동 후 boolean 값 변경*/}
+            {true ? <TurnedInIcon sx={{ fontSize: '60px', color: 'main.or', position: 'absolute', top: -2, left: -5, zIndex: 1, }}/>
+                : <TurnedInNotIcon sx={{ fontSize: '60px', color: 'main.or', position: 'absolute', top: -2, left: -5, zIndex: 1, }}/>}
+        </div>
+    );
+}
+
+export default Bookmark;
