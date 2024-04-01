1559d8a4287eab4de0b078221d5eea8500a96894
Joy <wlsgml235@gmail.com>
2024-04-02 00:52:33 +0900

    Design 마이페이지 디자인 수정

--- frontend/src/Profile/Input/Input.jsx
+++ frontend/src/Profile/Input/Input.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import "./Input.css"
 
-const Input = () => {
+const Input = ({placeholder}) => {
   return (
     <>
-    <input className="input" type='text'>
-    </input>
+    <input className="input" type='text' placeholder={placeholder}/>
     </>
   );
 }
