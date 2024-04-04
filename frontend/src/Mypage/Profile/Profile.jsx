fbdc8f39d7dbb2a3f3a3e60668d0f5b14538aeec
Joy <wlsgml235@gmail.com>
2024-04-05 00:38:46 +0900

    Design 프로필화면 완성

--- frontend/src/Mypage/Profile/Profile.jsx
+++ frontend/src/Mypage/Profile/Profile.jsx
@@ -1,71 +1,115 @@
-import React from 'react'
-import Input from '../../Component/Input/Input';
-import Inputbutton from '../../Component/Input/Inputbutton';
-import PersonIcon from '@mui/icons-material/Person';
-import Tooltip from '@mui/material/Tooltip';
-import "./ProfileImage.css"
-import Withdrawl from './Withdraw'
-import Sidebar from '../Sidebar'
-import "./Profile.css"
-
+import React from "react";
+import Input from "../../Component/Input/Input";
+import Inputbutton from "../../Component/Input/Inputbutton";
+import PersonIcon from "@mui/icons-material/Person";
+import Tooltip from "@mui/material/Tooltip";
+import "./ProfileImage.css";
+import Withdrawl from "./Withdraw";
+import Sidebar from "../Sidebar";
+import "./Profile.css";
 
 const Profile = () => {
   return (
-    <>
-    
-    {/* 사이드바 */}
-    <div className = 'sidebar'>
-      <Sidebar />
-    </div>
+    <div className="body">
+      {/* 사이드바 */}
+      <div className="sidebar">
+        <Sidebar />
+      </div>
 
-
-    {/* 프로필 */}
-    <div className = 'profile'>
-        <div className="ProfileImage">
-           <PersonIcon sx={{ color: 'black', width: '100px', height: '100px', position: 'absolute'}}/>{/* '#FFFF00'은 노란색을 나타냄 */}
-        </div>
-      <Inputbutton text="사진 변경" i= {true} w="medium"/>
-        
-        {/* 툴팁 */}
-          <Tooltip title="파일을 첨부해주세요" followCursor>
-          <div className="ProfileImage" sx={{ p: 2 }}>
-          <PersonIcon sx={{ color: 'black', width: '100px', height: '100px', position: 'absolute'}}/>{/* '#FFFF00'은 노란색을 나타냄 */}
+      {/* 프로필 */}
+      <div className="profile">
+        <div className="userprofile">
+          <div className="ProfileImage">
+            <PersonIcon
+              sx={{
+                color: "black",
+                width: "100px",
+                height: "100px",
+                position: "absolute",
+              }}
+            />
           </div>
-         </Tooltip>
-
-        <Inputbutton text="쉐프 신청" i= {true} w="medium"/>
-    </div>
-
-
-    {/* 폼형태 */}
-    <div className = 'form'>
-        아이디
-        <Input placeholder="jean1030"/>
-        닉네임
-        <Input placeholder="치킨나라피자공주"/>
-        <Inputbutton text="중복확인" i= {true} w="large" />
-        휴대폰번호
-        <Input placeholder="010-8495-9515"/>
-        이메일
-        <Input placeholder="jean1030@naver.com"/>
-        <Inputbutton text="이메일 인증" i= {true} w="large" />
-        비밀번호
-        <Input placeholder="*******"/>
-        새 비밀번호 변경
-        <Input placeholder="*******"/>
-        새 비밀번호 확인
-        <Input placeholder="*******"/>
+          <div className="changephoto">
+            <Inputbutton text="사진 변경" i={true} w="medium" />
+          </div>
         </div>
 
-        {/* 변경버튼 */}
-        <Inputbutton text="변경" i= {true} w="medium" />
+        {/* 툴팁 */}
+        <div className="chefprofile">
+          <Tooltip title="파일을 첨부해주세요" followCursor>
+            <div className="ProfileImage" sx={{ p: 2 }}>
+              <PersonIcon
+                sx={{
+                  color: "black",
+                  width: "100px",
+                  height: "100px",
+                  position: "absolute",
+                }}
+              />
+            </div>
+          </Tooltip>
+        </div>
 
-        {/* 회원 탈퇴 버튼 */}
-        <Withdrawl/>
+        <Inputbutton text="쉐프 신청" i={true} w="medium" />
+      </div>
 
-    </>
+      {/* 폼형태 */}
+      <div className="id">
+        아이디
+        <div className="id-input">
+          <Input placeholder="jean1030" />
+        </div>
+        <div className="nickname-button">
+          <div className="nickname">
+            <div className="duplicate-check">
+              닉네임
+              <span className="duplicate">
+                <Inputbutton text="중복확인" i={true} w="large" />
+              </span>
+            </div>
+            <Input placeholder="치킨나라피자공주" />
+          </div>
+        </div>
+        휴대폰번호
+        <div className="phone">
+          <Input placeholder="010-8495-9515" />
+        </div>
+        <div className="duplicate-check">
+          이메일
+          <span className="email">
+            <Inputbutton text="이메일 인증" i={true} w="large" />
+          </span>
+        </div>
+        <Input placeholder="jean1030@naver.com" />
+      </div>
+
+      <div className="pw">
+        비밀번호
+        <div className="password">
+          <Input placeholder="*******" />
+        </div>
+        새 비밀번호 변경
+        <div className="changepassword">
+          <Input placeholder="*******" />
+        </div>
+        새 비밀번호 확인
+        <div className="checkpassword">
+          <Input placeholder="*******" />
+        </div>
+      </div>
+
+      {/* 변경버튼 */}
+
+      <div className="change">
+        <Inputbutton text="변경" i={true} w="medium" />
+      </div>
+
+      {/* 회원 탈퇴 버튼 */}
+      <div className="withdrawl">
+        <Withdrawl />
+      </div>
+    </div>
   );
-}
-
+};
 
 export default Profile;
