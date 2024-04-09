75961daa992af103fb4c9cf4a645cf44fa9630dc
김희진 <95160590+Khjin06k@users.noreply.github.com>
2024-04-09 23:42:49 +0900

    Refactor: 리덕스 세팅 변경

---
+++ frontend/src/persistStore.jsx
@@ -0,0 +1,36 @@
+import {configureStore} from "@reduxjs/toolkit";
+import {persistReducer} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+export const initialState = {
+    member:{memberNo:''},
+    accessToken:'',
+}
+
+const reducer = (currentState,action) => {
+    if(currentState===undefined) {
+        return initialState;
+    }
+    const newState = {...currentState};
+    switch(action.type) {
+        case "member": newState.member=action.payload; break;
+        case "accessToken": newState.accessToken=action.payload; break;
+        default: 
+    }
+    return newState;
+}
+
+const persistConfig = {
+    key:'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+const store = configureStore({reducer:{persistedReducer},
+    middleware:(getDefaultMiddleware)=> 
+        getDefaultMiddleware({
+            serializableCheck:false,
+        })
+    });
+
+export default store;
