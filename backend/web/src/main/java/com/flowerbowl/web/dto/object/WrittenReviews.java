9b12d0a893312c7939df234f9b365208a93704f1
peeljunKim <135033836+peeljunKim@users.noreply.github.com>
2024-04-11 02:42:12 +0900

    Feat: Add 작성한 리뷰 조회, 리뷰 작성 항목 조회

---
+++ backend/web/src/main/java/com/flowerbowl/web/dto/object/WrittenReviews.java
@@ -0,0 +1,19 @@
+package com.flowerbowl.web.dto.object;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+public class WrittenReviews {
+
+    private String lesson_title;
+    private String lesson_writer;
+    private Long review_no;
+    private String review_date;
+    private Integer review_score;
+}
