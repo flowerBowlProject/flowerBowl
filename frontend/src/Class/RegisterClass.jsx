de2075ae82f3f9f55ae00ce7c2d9607573b4a9e5
김희진 <95160590+Khjin06k@users.noreply.github.com>
2024-04-11 00:25:34 +0900

    Design: 클래스 등록 css 수정 및 기능 정리

--- frontend/src/Class/RegisterClass.jsx
+++ frontend/src/Class/RegisterClass.jsx
@@ -3,26 +3,29 @@ import './RegisterClassStyle.css';
 
 import Category from '../Component/Category';
 import ToastEditor from "../Component/ToastEditor";
-
+import Button_contain_style from '../Component/Button_contain_style';
+import Button_outlined_style from '../Component/Button_outlined_style';
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { TextField } from "@mui/material";
-
 import AddressSearch from "./AddressSearch";
-import Inputbutton from "../Component/Input/Inputbutton";
+import  axios  from "axios";
+import { url } from "../url";
+import { useSelector } from 'react-redux';
 
 const RegisterClass = () => {
+    const accessToken = useSelector(state => state.persistedReducer.accessToken);
     {/* 등록 클래스 데이터 + 썸네일 + 썸네일 선택 여부 */ }
     const [registerData, setRegisterData] = useState({
-        lesson_title: '', lesson_category: '', lesson_price: 0,
-        lesson_address: '', lesson_contents: '', lesson_longitude: 0.0, lesson_latitude: 0.0, lesson_start: '', lesson_end: '', lesson_URL:''
+        lesson_title: '', lesson_category: '', lesson_price: 0, lesson_sname:'', lesson_oname:'',
+        lesson_address: '', lesson_content: '', lesson_longitude: 0.0, lesson_latitude: 0.0, lesson_start: '', lesson_end: '', lesson_URL: ''
     });
     const [thumbnail, setThumbnail] = useState(null);
     const [selectedFile, setSelectedFile] = useState(null);
 
+    {/* 썸네일 선택 및 변경 - 사진 업로드 과정 추가 필요 */}
     const chooseThumbnail = (e) => {
         const file = e.target.files[0];
 
@@ -38,17 +41,47 @@ const RegisterClass = () => {
 
     {/* 선택한 카테고리 값 받아와 저장 */ }
     const getCategory = category => {
-        console.log(category);
         setRegisterData((registerData) => ({ ...registerData, lesson_category: category }));
     }
 
     {/* 주소 및 위도/경도 받아와 저장 */ }
     const getAddress = address => {
-        console.log(address);
         setRegisterData((registerData) => ({ ...registerData, lesson_address: address.lesson_address }));
         setRegisterData((registerData) => ({ ...registerData, lesson_longitude: address.lesson_longitude }));
         setRegisterData((registerData) => ({ ...registerData, lesson_latitude: address.lesson_latitude }));
+    }
+
+    {/* 토스트 에디터 값 받아와 저장 */}
+    const getToastEditor = content =>{
+        setRegisterData((registerData) => ({ ...registerData, lesson_content: content}));
+    }
+
+    {/* 그 외의 변경사항 적용 */}
+    const setValue = (e) =>{
+        const value = e.target.value;
+        const name = e.target.name;
+        setRegisterData((registerData) => ({ ...registerData, [name]: value }));
+    }
+
+    {/* 클래스 등록 */}
+    const handleRegister = () =>{
         console.log(registerData);
+        axios.post(`${url}/api/lesson`, registerData, {
+            headers: {
+                Authorization: accessToken
+            }
+        })
+        .then(res=>{
+            console.log(res);    
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+    }
+
+    {/* 취소 버튼 클릭 */}
+    const handleCancel = () =>{
+        // 뒤로가기 - 리스트 페이지로 이동
     }
 
     return (
@@ -67,7 +100,7 @@ const RegisterClass = () => {
                             <input type='file' name='thumbImg' onChange={chooseThumbnail} />
                         </label>)}
                 </div>
-                <input className="register-title" type='text' placeholder="제목을 작성해 주세요." />
+                <input className="register-title" type='text' placeholder="제목을 작성해 주세요." name="lesson_title" onChange={(e)=> setValue(e)}/>
 
                 {/* 카테고리 선택 */}
                 <Category getCategory={getCategory} />
@@ -78,7 +111,7 @@ const RegisterClass = () => {
                 {/* 가격 */}
                 <div className="classPrice">
                     <TextField id="outlined-basic" label="가격" variant="outlined" type="number"
-                        sx={{ width: '12vw' }} onChange={(value) => setRegisterData((registerData) => ({ ...registerData, lesson_price: value }))} />
+                        sx={{ width: '12vw' }} name="lesson_price" onChange={(e)=> setValue(e)} />
                 </div>
                 {/* 날짜 */}
                 <div className="calendar-Box">
@@ -98,19 +131,22 @@ const RegisterClass = () => {
 
             {/* 장소 */}
             <div className="classElement2-Box">
-
-            <AddressSearch getAddress={getAddress} />
-            <TextField id="outlined-basic" type="text" label="오픈 채팅" variant="outlined"readOnly sx={{marginLeft:'1vw', width: '40vw'}} placeholder='오픈채팅 링크를 입력해 주세요.'
-        onChange={(newValue) => setRegisterData((registerData) => ({ ...registerData, lesson_URL: newValue }))}/>
+                <AddressSearch getAddress={getAddress} />
+                <TextField id="outlined-basic" type="text" label="오픈 채팅" variant="outlined" readOnly sx={{ marginLeft: '1vw', width: '40vw' }} placeholder='오픈채팅 링크를 입력해 주세요.'
+                    name="lesson_URL" onChange={(e)=> setValue(e)} />
             </div>
 
-
             {/* 레시피 || 클래스 상세 내용 작성란 */}
-            <ToastEditor />
-            {/* 조이님이 만들어주신 공통 컴포넌트 버튼 추가 필요 -> 등록 / 취소 버튼 */}
+            <ToastEditor getToastEditor={getToastEditor}/>
+
+            <div style={{ border: "1px solid #CBA285", marginBottom: "2%" }} />
             <div className="register_button">
-                <Inputbutton variant="outlined" text="등록" i= {false} w="medium-large"/> &nbsp;
-                <Inputbutton variant="outlined" text="취소" i= {true} w="medium-large"/>
+                <Button_outlined_style width='5vw' variant='outlined' onClick={handleRegister()}>
+                    등록
+                </Button_outlined_style> &nbsp;
+                <Button_contain_style width='5vw' variant='contained' onClick={handleCancel()}>
+                    취소
+                </Button_contain_style>
             </div>
         </div>
     );
--- frontend/src/Component/ToastEditor.jsx
+++ frontend/src/Component/ToastEditor.jsx
@@ -1,13 +1,22 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import { Editor } from '@toast-ui/react-editor';
 
-const ToastEditor = () => {
+const ToastEditor = (props) => {
     const editorRef = useRef();
+    const [content, setContent] = useState('');
+
+    const changeContent = () =>{
+        const data = editorRef.current.getInstance().getHTML();
+        setContent(data);
+        props.getToastEditor(data);
+        console.log(data);
+    }
 
     return (
-        <div className='editor' style={{margin: "1% auto", height:'500px'}}>
+        <div className='editor' style={{margin: "2% auto", height:'500px'}}>
             <Editor
+                onChange={changeContent}
                 placeholder="내용을 입력해주세요."
                 initialValue=' '
                 ref={editorRef}
@@ -16,6 +25,8 @@ const ToastEditor = () => {
                 initialEditType='wysiwyg'
                 hideModeSwitch={true}
             />
+
+            
         </div>
     );
 
