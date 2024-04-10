3dc52903e239f800da8442e2d53880f81f750544
peeljunKim <135033836+peeljunKim@users.noreply.github.com>
2024-04-11 00:46:16 +0900

    Revert "Feat: Add 북마크 목록 조회(레시피)"
    This reverts commit 42e689c06931df6eba878c48f76fb17dbbeeff68.

--- backend/web/src/main/java/com/flowerbowl/web/service/MypageService.java
+++ backend/web/src/main/java/com/flowerbowl/web/service/MypageService.java
@@ -1,7 +1,6 @@
 package com.flowerbowl.web.service;
 
-import com.flowerbowl.web.dto.response.mypage.GetLessonLikeListResponseDto;
-import com.flowerbowl.web.dto.response.mypage.GetRecipeLikeListResponseDto;
+import com.flowerbowl.web.dto.response.mypage.GetLessonLikeListResponseDTO;
 import org.springframework.http.ResponseEntity;
 
 public interface MypageService {
@@ -10,12 +9,5 @@ public interface MypageService {
      * 
      * @return  유저가 북마크한 lesson 리스트
      */
-    ResponseEntity<? super GetLessonLikeListResponseDto> getLessonLikeList(String userId);
-
-
-    /**
-     *
-     * @return 유저가 북마크한 recipe 리스트
-     */
-    ResponseEntity<? super GetRecipeLikeListResponseDto> getRecipeLikeList(String userId);
+    ResponseEntity<? super GetLessonLikeListResponseDTO> getLessonLikeList(String userId);
 }
