b2bccf0a7be3b7dd5040c27c7af6e65013a5589b
peeljunKim <135033836+peeljunKim@users.noreply.github.com>
2024-04-11 02:51:36 +0900

    Feat: Add 자신의 레시피 및 클래스 조회

---
+++ backend/web/src/main/java/com/flowerbowl/web/dto/object/MyRecipes.java
@@ -0,0 +1,19 @@
+package com.flowerbowl.web.dto.object;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+public class MyRecipes {
+
+    private String recipe_date;
+    private String recipe_title;
+    private Long bookmark_cnt;
+    private Long comment_cnt;
+
+}
