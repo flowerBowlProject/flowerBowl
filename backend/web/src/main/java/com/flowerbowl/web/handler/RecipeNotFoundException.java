874a3df89c68e70618e3e041d081bd5abed11bf3
ChangHoon <nick6253@naver.com>
2024-04-10 15:51:18 +0900

    Refactor: recipe, user null 체크 추가
    - findByRecipeNo()의 반환값에 Optional 추가로 없는 recipe 조회 시 RecipeNotFoundException 실행하도록 구현
    - findByUserNo()의 반환값에 Optional 추가로 없는 user 조회 시
    UserNotFoundException 실행하도록 구현

---
+++ backend/web/src/main/java/com/flowerbowl/web/handler/RecipeNotFoundException.java
@@ -0,0 +1,4 @@
+package com.flowerbowl.web.handler;
+
+public class RecipeNotFoundException extends RuntimeException {
+}
--- backend/web/src/main/java/com/flowerbowl/web/repository/RecipeRepository.java
+++ backend/web/src/main/java/com/flowerbowl/web/repository/RecipeRepository.java
@@ -3,10 +3,10 @@ package com.flowerbowl.web.repository;
 import com.flowerbowl.web.domain.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
 
-    Recipe findByRecipeNo(Long recipeNo);
-
-    void deleteByRecipeNo(Long recipeNo);
+    Optional<Recipe> findByRecipeNo(Long recipeNo);
 
 }
