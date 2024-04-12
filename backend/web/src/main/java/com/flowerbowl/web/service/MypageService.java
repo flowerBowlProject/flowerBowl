73dd00bd62f433f958baf37c79ff557c53e9dbf5
peeljunKim <135033836+peeljunKim@users.noreply.github.com>
2024-04-12 14:45:24 +0900

    Feat: 북마크(레시피) 조회

--- backend/web/src/main/java/com/flowerbowl/web/service/MypageService.java
+++ backend/web/src/main/java/com/flowerbowl/web/service/MypageService.java
@@ -1,6 +1,7 @@
 package com.flowerbowl.web.service;
 
 import com.flowerbowl.web.dto.response.mypage.GetLessonLikesResponseDto;
+import com.flowerbowl.web.dto.response.mypage.GetRecipeLikesResponseDto;
 import org.springframework.http.ResponseEntity;
 
 public interface MypageService {
@@ -9,5 +10,11 @@ public interface MypageService {
      * 
      * @return  유저가 북마크한 lesson 리스트
      */
-    ResponseEntity<? super GetLessonLikesResponseDto> getLessonLikeList(String userId);
+    ResponseEntity<? super GetLessonLikesResponseDto> getLessonLikes(String userId);
+
+    /**
+     * 
+     * @return 유저가 북마크한 recipe 리스트
+     */
+    ResponseEntity<? super GetRecipeLikesResponseDto> getRecipeLikes(String userId);
 }
--- backend/web/src/main/java/com/flowerbowl/web/service/implement/MypageServiceImpl.java
+++ backend/web/src/main/java/com/flowerbowl/web/service/implement/MypageServiceImpl.java
@@ -1,14 +1,12 @@
 package com.flowerbowl.web.service.implement;
 
-import com.flowerbowl.web.domain.Lesson;
-import com.flowerbowl.web.domain.LessonLike;
-import com.flowerbowl.web.domain.User;
+import com.flowerbowl.web.domain.*;
 import com.flowerbowl.web.dto.object.mypage.LikeLessons;
+import com.flowerbowl.web.dto.object.mypage.LikeRecipes;
 import com.flowerbowl.web.dto.response.ResponseDto;
 import com.flowerbowl.web.dto.response.mypage.GetLessonLikesResponseDto;
-import com.flowerbowl.web.repository.LessonLikeRepository;
-import com.flowerbowl.web.repository.LessonRepository;
-import com.flowerbowl.web.repository.UserRepository;
+import com.flowerbowl.web.dto.response.mypage.GetRecipeLikesResponseDto;
+import com.flowerbowl.web.repository.*;
 import com.flowerbowl.web.service.MypageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -27,12 +25,15 @@ public class MypageServiceImpl implements MypageService {
     private final UserRepository userRepository;
     private final LessonLikeRepository lessonLikeRepository;
     private final LessonRepository lessonRepository;
+    private final RecipeLikeRepository reviewLikeRepository;
+    private final RecipeRepository recipeRepository;
+
 
     /**
      * 개인적으로 로직이 너무 아쉬움 view 테이블로 하면 좋을 거 같음 나중에 리팩토링해보자
      */
     @Override
-    public ResponseEntity<? super GetLessonLikesResponseDto> getLessonLikeList(String userId) {
+    public ResponseEntity<? super GetLessonLikesResponseDto> getLessonLikes(String userId) {
 
         List<LikeLessons> lessons = new ArrayList<>();
 
@@ -65,4 +66,41 @@ public class MypageServiceImpl implements MypageService {
 
         return GetLessonLikesResponseDto.success(lessons);
     }
+
+    @Override
+    public ResponseEntity<? super GetRecipeLikesResponseDto> getRecipeLikes(String userId) {
+
+        List<LikeRecipes> likeRecipes = new ArrayList<>();
+
+        try {
+
+            User user = userRepository.findByUserId(userId);
+            Long userNo = user.getUserNo();
+            List<RecipeLike> recipeLikes = reviewLikeRepository.findAllByUser_UserNo(userNo);
+            if(recipeLikes == null) GetRecipeLikesResponseDto.noExistRecipe();
+
+            List<Long> recipeNos = new ArrayList<>();
+            for (RecipeLike list : recipeLikes) {
+                recipeNos.add(list.getRecipe().getRecipeNo());
+            }
+
+            List<Recipe> recipes = recipeRepository.findAllByRecipeNoIn(recipeNos);
+            for (Recipe recipe : recipes){
+                LikeRecipes likeRecipesByUser = new LikeRecipes();
+                likeRecipesByUser.setRecipe_no(recipe.getRecipeNo());
+                likeRecipesByUser.setRecipe_title(recipe.getRecipeTitle());
+                likeRecipesByUser.setRecipe_oname(recipe.getRecipeOname());
+                likeRecipesByUser.setRecipe_sname(recipe.getRecipeSname());
+                likeRecipes.add(likeRecipesByUser);
+            }
+
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+        return GetRecipeLikesResponseDto.success(likeRecipes);
+    }
+
+
 }
