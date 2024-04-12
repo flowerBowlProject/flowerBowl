ac3466a0a96e5344377326197a561c2cc4a56a97
peeljunKim <135033836+peeljunKim@users.noreply.github.com>
2024-04-12 18:57:11 +0900

    Rename: RecipeRepository 위치 변경

--- backend/web/src/main/java/com/flowerbowl/web/repository/RecipeRepository.java
+++ backend/web/src/main/java/com/flowerbowl/web/repository/RecipeRepository.java
@@ -1,12 +1,14 @@
-package com.flowerbowl.web.service.implement;
+package com.flowerbowl.web.repository;
 
 import com.flowerbowl.web.domain.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-
+import java.util.Optional;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+
+    Optional<Recipe> findByRecipeNo(Long recipeNo);
 
     List<Recipe> findAllByRecipeNoIn(List<Long> recipeNos);
 }
