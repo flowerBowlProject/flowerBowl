df69188ae7f47aeadc94eac283c1830ac3382870
김희진 <95160590+Khjin06k@users.noreply.github.com>
2024-04-12 21:05:50 +0900

    Design: 커뮤니티 즐겨찾기 삭제

--- frontend/src/Community/CommunityDetail.jsx
+++ frontend/src/Community/CommunityDetail.jsx
@@ -39,9 +39,6 @@ const CommunityDetail = () => {
             <div className="communityDetail-Box">
                 <div className="community-title"> {communityData.community_title} </div>
                 <div className='community-body'>{communityData.community_content}</div>
-                {/* 즐겨찾기 버튼 - 즐겨찾기 여부에 따른 true / false로 아이콘 표시 */}
-                <div className="community-bookmark">{true ? <TurnedInNotIcon sx={{ fontSize: '60px', color: 'main.or'}}/> : 
-                <TurnedInIcon sx={{ fontSize: '60px', color: 'main.or'}}/>} 스크랩 </div>
                 {/* 수정/삭제 버튼 - 작성자인 경우에만 true로 버튼 표시 */}
                 <div className="community-change">
                     {true &&  <Button_outlined_style width='5vw' sx={{height:"2vw"}} variant='outlined' onClick={handleModify}>수정</Button_outlined_style>} &nbsp;
