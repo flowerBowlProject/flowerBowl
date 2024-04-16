9a9774242799d4dabbcb0955dbe83074f808807c
Yeongchang Park <hazedayz0223@gmail.com>
2024-04-16 16:50:06 +0900

    design : 로그인 회원가입 모달 css 수정, header, footer navigate 추가

--- frontend/src/LoginModal/CommonModal.jsx
+++ frontend/src/LoginModal/CommonModal.jsx
@@ -18,15 +18,15 @@ const ButtonLoginStyle =styled(Button)(({theme})=>({
 const CommonModal=({open,name_1,name_2,helpertext_1,helpertext_2,but_name,text_1,text_2,text_3,api_login,type_pass,handleOpen})=>{
     return(
         <Modal open={open}>
-            <Box bgcolor='main.br' mt='10vw' mx='25vw' border='3px solid #F6C47B' borderRadius={1} width='50vw' height='30vw'>
+            <Box bgcolor='main.br' mt='10vw' mx='30vw' border='3px solid #F6C47B' borderRadius={1} width='40vw' height='30vw'>
                 <Grid container direction='column' mt='2vw'>
                     <InputMember name={name_1} helpertext={helpertext_1}/>
                     <InputMember type_pass={type_pass} name={name_2} helpertext={helpertext_2}/>
-                    <Grid item xs>
-                        <Button_contain_style width='15vw' sx={{ml:'12vw',mb:'2vw'}}>{but_name}</Button_contain_style>
+                    <Grid item xs textAlign='center'>
+                        <Button_contain_style width='15vw' sx={{mb:'2vw'}}>{but_name}</Button_contain_style>
                     </Grid>
-                    <Grid item xs>
-                        <ButtonGroup  color='secondary' variant='text'  sx={{color:'#ffffff',marginLeft:'13.5vw',mb:'2vw'}}>
+                    <Grid item xs textAlign='center' >
+                        <ButtonGroup  color='secondary' variant='text'  sx={{color:'#ffffff',mb:'2vw'}}>
                             <ButtonLoginStyle onClick={handleOpen}>{text_1}</ButtonLoginStyle>
                             <ButtonLoginStyle onClick={handleOpen}>{text_2}</ButtonLoginStyle>
                             <ButtonLoginStyle onClick={handleOpen}>{text_3}</ButtonLoginStyle>
@@ -34,11 +34,11 @@ const CommonModal=({open,name_1,name_2,helpertext_1,helpertext_2,but_name,text_1
                     </Grid>
                     {api_login?
                     <Grid item xs  mb='3vw' >
-                        <Grid container>
-                        <Grid item xs>
+                        <Grid container justifyContent='center'>
+                        <Grid item xs textAlign='right'>
                             <img src='../../images/But_kakao.png'   className='kakao' height='40vw'/>
                         </Grid>
-                        <Grid item xs >
+                        <Grid item xs  textAlign='left'>
                             <img src='../../images/But_naver.png'  className='naver' width='150px'  />
                         </Grid>
                         </Grid>
