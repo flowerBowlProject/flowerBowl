12ffb9780d33cb0fff4042b83cb24b31c151a192
Yeongchang Park <hazedayz0223@gmail.com>
2024-04-16 21:01:12 +0900

    Design: 모달수정 헤더 수정

--- frontend/src/LoginModal/InputMember.jsx
+++ frontend/src/LoginModal/InputMember.jsx
@@ -4,11 +4,11 @@ import InputMemberStyle from './InputMemberStyle';
 const InputMember=({name,helpertext,type_pass})=>{
     return(
         <Grid container direction='row' justifyContent='center'  my='2vw'>
-            <Grid item xs={5}>
+            <Grid item xs={4}>
                 <Typography variant='h5' color='#ffffff' align='center' ml='6vw'>{name}</Typography>
             </Grid>
-            <Grid item xs={4} mr='8vw' height='3vw' > 
-                <InputMemberStyle type={type_pass?'password':null}  variant='standard'  helperText={helpertext} fullWidth/>
+            <Grid item xs={5} mr='8vw' height='3vw' > 
+                <InputMemberStyle type={type_pass?'password':null}  variant='standard'  helperText={helpertext} sx={{width:'17.5vw'}}/>
             </Grid>
         </Grid>
     )
