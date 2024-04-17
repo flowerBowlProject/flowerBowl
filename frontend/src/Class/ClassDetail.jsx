6f4b9393c4db73775fb2f68f3d2185c89e1474a8
김희진 <95160590+Khjin06k@users.noreply.github.com>
2024-04-17 17:25:36 +0900

    Feat: 클래스 전체 조회 및 디테일 페이지 동작 구현 (비로그인)

--- frontend/src/Class/ClassDetail.jsx
+++ frontend/src/Class/ClassDetail.jsx
@@ -2,65 +2,70 @@ import React, { useEffect, useState } from "react";
 import './ClassDetailStyle.css';
 import TurnedInNotIcon from '@mui/icons-material/TurnedInNot';
 import TurnedInIcon from '@mui/icons-material/TurnedIn';
-import  axios  from "axios";
+import axios from "axios";
 import { useLocation } from "react-router";
 import { url } from "../url";
 import ButtonContain from "../Component/ButtonContain";
+import { useSelector } from "react-redux";
+import { useParams } from 'react-router-dom';
+import ButtonOutlined from "../Component/ButtonOutlined";
+
 
 const { kakao } = window;
 
 const ClassDetail = () => {
-    const [classData, setClassData] = useState({
-        lesson_title: '클래스 제목', lesson_sname: '', lesson_writer: '작성자1', lesson_price: 1000, lesson_URL: 'http://kakao.chat/102552',
-        lesson_start: '2024-03-19', lesson_end: '2024-04-19', lesson_category: '밥', lesson_content: '클래스 내용', lesson_like_status: false,
-        lesson_addr: '서울특별시 강서구', lesson_longitude: 126.9779692, lesson_latitude: 37.566535
-    });
-
-    const location = useLocation();
-    const lesson_no = location.state?.lesson_no;
+    const [classData, setClassData] = useState({});
+    const { lesson_no } = useParams();
+    const [writer, setWriter] = useState(false);
+    const accessToken = useSelector(state => state.persistedReducer.accessToken);
 
     useEffect(() => {
-        // 이미지 지도에서 마커가 표시될 위치입니다 
-        var markerPosition = new kakao.maps.LatLng(classData.lesson_latitude,classData.lesson_longitude);
+        if (accessToken === '') {
+            axios.get(`${url}/api/guest/lessons/${lesson_no}`)
+                .then(res => {
+                    console.log(res);
+                    setClassData(res.data.lesson);
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        } else {
 
-        // 이미지 지도에 표시할 마커입니다
-        // 이미지 지도에 표시할 마커는 Object 형태입니다
-        var marker = {
-            position: markerPosition
-        };
+        }
+    }, [lesson_no])
 
-        var staticMapContainer = document.getElementById('staticMap'), // 이미지 지도를 표시할 div  
-            staticMapOption = {
-                center: new kakao.maps.LatLng(classData.lesson_latitude,classData.lesson_longitude), // 이미지 지도의 중심좌표
-                level: 3, // 이미지 지도의 확대 레벨
-                marker: marker // 이미지 지도에 표시할 마커 
+    useEffect(() => {
+        var mapContainer = document.getElementById('staticMap'), // 지도를 표시할 div 
+            mapOption = {
+                center: new kakao.maps.LatLng(classData.lesson_latitude, classData.lesson_longitude), // 지도의 중심좌표
+                level: 3 // 지도의 확대 레벨
             };
 
-        // 이미지 지도를 생성합니다
-        var staticMap = new kakao.maps.StaticMap(staticMapContainer, staticMapOption);
-    }, [])
+        var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
-    useEffect(()=>{
-        {/* 정보 가져오기 - 로그인 여부에 따라 요청 axios 호출 조건문 필요 */}
-        axios.get(`${url}/lesson/user/${lesson_no}`)
-        .then(res=>{
+        // 마커가 표시될 위치입니다 
+        var markerPosition = new kakao.maps.LatLng(classData.lesson_latitude, classData.lesson_longitude);
 
-        })
-        .catch(err=>{
+        // 마커를 생성합니다
+        var marker = new kakao.maps.Marker({
+            position: markerPosition
+        });
 
-        })
-    },[])
+        // 마커가 지도 위에 표시되도록 설정합니다
+        marker.setMap(map);
 
-    {/* 클래스 구매 */}
-    const buyClass = () =>{
+    }, [classData])
+
+    {/* 클래스 구매 */ }
+    const buyClass = () => {
     }
 
-    {/* 클래스 수정 */}
-    const handleModify = () =>{
+    {/* 클래스 수정 */ }
+    const handleModify = () => {
     }
 
-    {/* 클래스 삭제 */}
-    const handleDelete = () =>{
+    {/* 클래스 삭제 */ }
+    const handleDelete = () => {
     }
 
     return (
@@ -71,9 +76,9 @@ const ClassDetail = () => {
                     {/* 썸네일 첨부 필요 */}
                 </div>
                 <div className="class-element">
-                    <div style={{float:"left", textAlign:"center"}}>
+                    <div style={{ float: "left", textAlign: "center" }}>
                         <div className="class-title"> {classData.lesson_title} </div>
-                        <div className="class-category" style={{color:"#B9835C"}}> category : {classData.lesson_category} </div>
+                        <div className="class-category" style={{ color: "#B9835C" }}> category : {classData.lesson_category} </div>
                     </div>
                     <div className="class-writerDateURL">
                         <div>
@@ -85,23 +90,23 @@ const ClassDetail = () => {
 
                 {/* 가격 + 오픈채팅 링크 + 장소 (지도 api)*/}
                 <div className="class-map">
-                    <div id="staticMap" style={{ width: '100%', height: '20vw' }}></div>
+                    <div id="staticMap" style={{ height: '20vw' }}></div>
                 </div>
                 <div className="class-addr">
                     장소 : {classData.lesson_addr}
                 </div>
-                <div className='class-body'>{classData.lesson_content}</div>
+                <div className='class-body'>{classData.lesson_contents}</div>
 
                 {/* 즐겨찾기 버튼 - 즐겨찾기 여부에 따른 true / false로 아이콘 표시 */}
-                <div className="class-bookmark">{classData.lesson_like_status ? <TurnedInNotIcon sx={{ fontSize: '60px', color: 'main.or' }} /> :
-                    <TurnedInIcon sx={{ fontSize: '60px', color: 'main.or' }} />} 스크랩 </div>
+                <div className="class-bookmark">{classData.lesson_likes_status === true ? <TurnedInIcon sx={{ fontSize: '60px', color: 'main.or' }} /> :
+                    <TurnedInNotIcon  sx={{ fontSize: '60px', color: 'main.or' }} />} 스크랩 </div>
 
                 {/* 수정/삭제 버튼 - 작성자인 경우에만 true로 버튼 표시 + 구매하기 버튼 - 작성자가 아닌 경우 노출 */}
                 <div className="class-change">
                     가격 : {classData.lesson_price} &nbsp;&nbsp;&nbsp;
-                    {false && <ButtonContain size='large' text='로그인'/>}
-                    {true &&  <ButtonContain size='large' text='로그인'/>} &nbsp;&nbsp;
-                    {true &&  <ButtonContain size='large' text='로그인'/>}
+                    {false && <ButtonContain size='large' text='구매' />}
+                    {true && <ButtonOutlined size='large' text='수정' />} &nbsp;&nbsp;
+                    {true && <ButtonContain size='large' text='삭제' />}
                 </div>
             </div>
         </>
