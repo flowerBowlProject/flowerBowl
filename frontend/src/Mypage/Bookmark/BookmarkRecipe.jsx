7218d6a1a58d83140a338bf4073c4247a7432217
프론트개발자 조이 <wlsgml235@gmail.com>
2024-04-18 22:01:07 +0900

    Feat: 프로필페이지 사진첨부 추가

--- frontend/src/Mypage/Bookmark/BookmarkRecipe.jsx
+++ frontend/src/Mypage/Bookmark/BookmarkRecipe.jsx
@@ -1,23 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CardComp from "../../Component/CardComp";
 import Bookmark from "../../Component/Bookmark";
 import ButtonContain from "../../Component/ButtonContain";
 import ButtonOutlined from "../../Component/ButtonOutlined";
 import "./BookmarkRecipe.css";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { url } from "../../url";
 
 const BookmarkRecipe = () => {
-  const sampleRecipeData = {
-    date: "2020-01-01",
-    comment_count: 123,
-    like_count: 456,
-    title: "시원한 소고기 무국",
-  };
+  const [listData, setListData] = useState([]);
+
+  useEffect(() => {
+    //로그인한 상태
+    axios
+      .get(`${url}/api/mypage/recipe/likes`)
+      .then((res) => {
+        console.log(res);
+        setListData(res.data.likeRecipes);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  // const sampleRecipeData = {
+  //   date: "2020-01-01",
+  //   comment_count: 123,
+  //   like_count: 456,
+  //   title: "시원한 소고기 무국",
+  // };
 
   const bookmarkComponents = Array.from({ length: 8 }, (_, index) => (
     <div key={index} className="bookmark-wrapper">
-      <Bookmark />
-      <CardComp list={sampleRecipeData} />
+      <Bookmark check={true} />
+      <CardComp list={listData} />
     </div>
   ));
 
