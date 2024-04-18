1b33e4fe2a8738769da006175cdefa5b7a9122a5
프론트개발자 조이 <wlsgml235@gmail.com>
2024-04-18 22:09:47 +0900

    Fix: 머지 충돌해결

--- frontend/src/Component/CardComp.jsx
+++ frontend/src/Component/CardComp.jsx
@@ -1,21 +1,21 @@
-import * as React from 'react';
-import './CardCompStyle.css';
-import Card from '@mui/material/Card';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import TurnedInIcon from '@mui/icons-material/TurnedIn';
-import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
-import { url } from '../url';
+import * as React from "react";
+import "./CardCompStyle.css";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import TurnedInIcon from "@mui/icons-material/TurnedIn";
+import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
+import { url } from "../url";
 
 // 전체적으로 list의 element의 단어들을 api와 일치시켜야 함
 const CardComp = ({title, like_count, date, comment_count, sname, onClick, type}) => {
   const handleClick = () =>{
     if(onClick) onClick();
   };
-  
+
   return (
     <div onClick={handleClick}>
-      <Card className='card-Box' >
+      <Card className="card-Box">
         {/* 이미지 요청 api로 변경 필요 */}
         <CardMedia
           component="img"
@@ -38,7 +38,7 @@ const CardComp = ({title, like_count, date, comment_count, sname, onClick, type}
           </div>
             }
           </div>
-          <div className='views-title'> {title} </div>
+          <div className="views-title"> {title} </div>
         </CardContent>
       </Card>
     </div>
