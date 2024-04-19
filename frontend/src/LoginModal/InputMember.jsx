1ec96fce4156963b673af89894640c7ae306b0fb
Yeongchang Park <hazedayz0223@gmail.com>
2024-04-20 01:56:08 +0900

    feat: 추가 로그인 api 구현

--- frontend/src/LoginModal/InputMember.jsx
+++ frontend/src/LoginModal/InputMember.jsx
@@ -2,11 +2,12 @@ import React,{useState} from 'react'
 import {Grid,Typography,Skeleton,TextField} from '@mui/material'
 import InputMemberStyle from './InputMemberStyle';
 import { validation } from '../Hook/Validation';
-const InputMember=({name,helpertext,type_pass,vaildTest,setBut=()=>{}})=>{
+const InputMember=({name,helpertext,type_pass,vaildTest,setBut=()=>{},setUser=()=>{}})=>{
     const [text,setText]= useState('');
     const handleText = (e)=>{
         const value=e.target.value;
         setText(value);
+        setUser(name,value);
         const tf=validation(value,vaildTest);
         setBut(tf);
     }
@@ -17,7 +18,7 @@ const InputMember=({name,helpertext,type_pass,vaildTest,setBut=()=>{}})=>{
                 <Typography variant='h5' color='#ffffff' align='center' ml='6vw'>{name}</Typography>
             </Grid>
             <Grid item xs={5} mr='8vw' height='3vw' > 
-                <InputMemberStyle type={type_pass?'password':null} onBlur={()=>{setValidTest(true)}}  variant='standard' onChange={handleText} helperText={validTest?validation(text,vaildTest)?helpertext:'':''}sx={{width:'17.5vw'}}  />
+                <InputMemberStyle type={type_pass?'password':null}  onBlur={()=>{setValidTest(true)}}  variant='standard' onChange={handleText} helperText={validTest?validation(text,vaildTest)?helpertext:'':''}sx={{width:'17.5vw'}}  />
             </Grid>
         </Grid>
     )
