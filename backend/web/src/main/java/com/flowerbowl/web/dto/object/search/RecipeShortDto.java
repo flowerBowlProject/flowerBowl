8231d53e3651eb476aa270f4c582d8d1e3e5b7de
H2ll0World <sunr1058@naver.com>
2024-04-21 00:14:06 +0900

    FEAT: 4/21 frontend 요청사항 추가

--- backend/web/src/main/java/com/flowerbowl/web/dto/object/search/RecipeShortDto.java
+++ backend/web/src/main/java/com/flowerbowl/web/dto/object/search/RecipeShortDto.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
 @Builder
@@ -14,6 +16,11 @@ public class RecipeShortDto {
     private String recipe_sname;
     private String recipe_oname;
     private String recipe_writer;
+    private LocalDate recipe_date;
+    // 따로 추가해줘야 하는 부분
+    private Long recipe_likes_num;
+    private Long recipe_comments_num;
+    private Boolean recipe_likes_status;
 
     public static RecipeShortDto from(Recipe recipe){
         return RecipeShortDto.builder()
@@ -22,6 +29,7 @@ public class RecipeShortDto {
                 .recipe_sname(recipe.getRecipeSname())
                 .recipe_oname(recipe.getRecipeOname())
                 .recipe_writer(recipe.getRecipeWriter())
+                .recipe_date(recipe.getRecipeDate())
                 .build();
     }
 }
--- backend/web/src/main/java/com/flowerbowl/web/repository/LessonLikeRepository.java
+++ backend/web/src/main/java/com/flowerbowl/web/repository/LessonLikeRepository.java
@@ -2,6 +2,7 @@ package com.flowerbowl.web.repository;
 
 
 import com.flowerbowl.web.domain.LessonLike;
+import org.hibernate.sql.ast.tree.predicate.BooleanExpressionPredicate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +12,8 @@ import java.util.List;
 public interface LessonLikeRepository extends JpaRepository<LessonLike, Long> {
 
     List<LessonLike> findAllByUser_UserNo(Long userNo);
+
+    Long countLessonLikeByLesson_LessonNo(Long lessonNo);
+
+    Boolean existsLessonLikeByLesson_LessonNoAndUser_UserId(Long lessonNo, String userId);
 }
