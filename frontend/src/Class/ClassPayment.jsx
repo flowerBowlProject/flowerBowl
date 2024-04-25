44c2e1007b642fcf4b3a09db235b9946356a0730
김희진 <95160590+Khjin06k@users.noreply.github.com>
2024-04-25 23:06:29 +0900

    Feat: 클래스 구매 연결

---
+++ frontend/src/Class/ClassPayment.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { url } from "../url";
+import { useSelector } from "react-redux";
+import ButtonContain from "../Component/ButtonContain";
+
+const { IMP } = window;
+
+const ClassPayment = ({ lesson_no }) => {
+    const [paymentData, setPaymentData] = useState({});
+    const accessToken = useSelector(state => state.accessToken);
+
+    const buyClass = async () => {
+        try {
+            const response = await axios.post(`${url}/api/user/lessons/payments`, {
+                "lesson_no": lesson_no
+            }, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+            console.log(response);
+            setPaymentData(response.data.payinfo);
+            if (window.IMP) {
+                window.IMP.init([response.data.payinfo.store_id]); // 결제 데이터 정의
+            }
+            window.IMP.request_pay(response.data.payinfo, callback);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const callback = (response) => {
+        const { success, error_msg, imp_uid, merchant_uid, pay_method, paid_amount, status } = response;
+        if (success) {
+            console.log('성공');
+        } else {
+            console.log('실패');
+        }
+    };
+
+    return (
+        <>
+            <ButtonContain size='large' text='구매' handleClick={buyClass}/>
+        </>
+    ); // 이 컴포넌트는 실제로 렌더링할 필요가 없으므로 null을 반환합니다.
+};
+
+export default ClassPayment;
