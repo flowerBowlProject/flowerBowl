3a5c437c0847f0071da104428ad8758e533ad0a6
ChangHoon <nick6253@naver.com>
2024-04-26 14:23:21 +0900

    Feat: 댓글 등록 API 구현
    - 댓글 등록 API 구현
    - mysql serverTimezone을 UTC에서 Seoul로 변경
    - ResponseCode와 ResponseMessage에 NOT_EXIST_COMMENT, WRONG_BOARD_TYPE 추가

--- backend/web/src/main/java/com/flowerbowl/web/domain/Comment.java
+++ backend/web/src/main/java/com/flowerbowl/web/domain/Comment.java
@@ -1,10 +1,18 @@
 package com.flowerbowl.web.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Comment {
 
     @Id
