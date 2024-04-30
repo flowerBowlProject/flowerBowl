68440cd947756c8816c28cba10e3b0548c008b63
peeljunKim <135033836+peeljunKim@users.noreply.github.com>
2024-04-30 17:17:57 +0900

    Refactor: 사용자 검증 오류 메시지 통일
    NOT_MATCH_USER -> DOES_NOT_MATCH으로 수정
    
    수정 목록
    1. 리뷰 수정
    2. 리뷰 삭제
    3. 리뷰 상세

--- backend/web/src/main/java/com/flowerbowl/web/dto/response/review/DeleteReviewResponseDto.java
+++ backend/web/src/main/java/com/flowerbowl/web/dto/response/review/DeleteReviewResponseDto.java
@@ -25,7 +25,7 @@ public class DeleteReviewResponseDto extends ResponseDto {
     }
 
     public static ResponseEntity<? super ResponseDto> notMatchUser() {
-        ResponseDto body = new ResponseDto(ResponseCode.NOT_MATCH_USER, ResponseMessage.NOT_MATCH_USER);
+        ResponseDto body = new ResponseDto(ResponseCode.DOES_NOT_MATCH, ResponseMessage.DOES_NOT_MATCH);
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(body);
     }
 }
