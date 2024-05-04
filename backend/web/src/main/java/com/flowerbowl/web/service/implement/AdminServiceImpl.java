59b07ad50b824cf094d71b75cee4495f6edb1022
H2ll0World <sunr1058@naver.com>
2024-05-04 14:34:32 +0900

    FEAT: 클래스 이미지 관련기능 추가, FEAT: 클래스 카테고리별 조회, 인기클래스 조회 추가

--- backend/web/src/main/java/com/flowerbowl/web/service/implement/AdminServiceImpl.java
+++ backend/web/src/main/java/com/flowerbowl/web/service/implement/AdminServiceImpl.java
@@ -8,7 +8,9 @@ import com.flowerbowl.web.dto.request.admin.BannerRequestDto;
 import com.flowerbowl.web.dto.object.admin.ChefCandidiateDto;
 import com.flowerbowl.web.dto.response.admin.ChefResponseDto;
 import com.flowerbowl.web.repository.admin.*;
+import com.flowerbowl.web.repository.banner.BannerJpaDataRepository;
 import com.flowerbowl.web.service.AdminService;
+import com.flowerbowl.web.service.ImageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -29,6 +31,7 @@ public class AdminServiceImpl implements AdminService {
     private final AdminLicenseJpaDataRepository adminLicenseJpaDataRepository;
     private final AdminUserJpaDataRepository adminUserJpaDataRepository;
     private final AdminBannerJpaDataRepository adminBannerJpaDataRepository;
+    private final ImageService imageService;
 
     // 쉐프 신청한 회원 전체 조회
     @Override
@@ -116,21 +119,37 @@ public class AdminServiceImpl implements AdminService {
     @Override
     public ResponseEntity<ResponseDto> banner(BannerRequestDto bannerRequestDto){
         try{
-            // 기존에 등록된 배너가 없는 경우
-            if(adminBannerJpaDataRepository.countAllByBannerNoGreaterThan(-1L) == 0){
-                Banner banner = new Banner();
-                banner.setBannerContent(bannerRequestDto.getAdmin_content());
-                banner.setBannerFileSname(bannerRequestDto.getAdmin_banner_sname());
-                banner.setBannerFileOname(bannerRequestDto.getAdmin_banner_oname());
-                adminBannerRepository.bannerRegister(banner);
-
-                return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto());
+            // oname : temp/banner/파일명,
+            String requestBannerOname = bannerRequestDto.getAdmin_banner_oname();
+            String requestBannerSname = bannerRequestDto.getAdmin_banner_sname();
+            if(requestBannerOname == null || requestBannerOname.trim().isEmpty() || requestBannerSname == null || requestBannerSname.trim().isEmpty()){
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseDto("FA", "request에 배너가 존재하지 않습니다."));
             }
-            // 기존에 등록된 배너가 있는 경우 기존의 배너를 바꿔줌
-            Banner banner = adminBannerRepository.findOne();
+            // 기존에 등록된 배너가 있는 경우
+            if(!(adminBannerJpaDataRepository.countAllByBannerNoGreaterThan(-1L) == 0)){
+                List<Banner> bannerList = adminBannerJpaDataRepository.findAll();
+                for(Banner iter : bannerList){
+                    // S3에서 삭제
+                    log.info(iter.getBannerFileOname());
+                    imageService.deleteS3(iter.getBannerFileOname());
+
+                    // DB에서 삭제
+                    adminBannerJpaDataRepository.delete(iter);
+                }
+            }
+
+            // banner/파일명
+            String newBannerOname = "banner/" + requestBannerOname.split("/")[2];
+            String newBannerSname = "https://flowerbowl.s3.ap-northeast-2.amazonaws.com/" + newBannerOname;
+            // S3에 넣어줌
+            imageService.copyS3(requestBannerOname, newBannerOname);
+
+            // DB에 넣어줌
+            Banner banner = new Banner();
             banner.setBannerContent(bannerRequestDto.getAdmin_content());
-            banner.setBannerFileSname(bannerRequestDto.getAdmin_banner_sname());
-            banner.setBannerFileOname(bannerRequestDto.getAdmin_banner_oname());
+            banner.setBannerFileSname(newBannerSname);
+            banner.setBannerFileOname(newBannerOname);
+            adminBannerRepository.bannerRegister(banner);
 
             return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto());
 
